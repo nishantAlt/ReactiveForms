b04ea985006e0e88c045c7aef448d61e1a085c70
NishantAlt <nishant.office.com@gmail.com>
2022-06-22 18:22:31 +0530

    video 7 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeeComponent } from './employee/list-employee.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employeeForm: FormGroup;
+
+  constructor() { 
+    this.employeeForm=new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl(),
+
+      skills: new FormGroup(
+        {
+          skillName: new FormControl(),
+          experienceInYears: new FormControl(),
+          proficiency: new FormControl()
+        }
+      )
+    })
+  }
+
+  ngOnInit(): void {}
+
+  onLoadDataClick() : void
+  {
+    this.employeeForm.setValue({
+      fullName: "Nishant Badlani",
+      email: "nishant@gmail.com",
+      skills:{
+        skillName: "C#",
+        experienceInYears: "0.5",
+        proficiency: "beginner"
+      }
+    })
+  }
+
+  onSubmit()
+  {
+    console.log(this.employeeForm.value);
+    console.log(this.employeeForm.controls['fullName'].value);
+  }
+}
