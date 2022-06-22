77965862381b964fec2ec1b4ff74bb19e2aaa4bd
Nishant Badlani <nishantbadlani2@gmail.com>
2022-06-22 23:15:19 +0530

    video 10 done, from pc

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -8,23 +8,31 @@ import { FormGroup,FormControl } from '@angular/forms';
 export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
-
-  constructor() { 
-    this.employeeForm=new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-
-      skills: new FormGroup(
-        {
-          skillName: new FormControl(),
-          experienceInYears: new FormControl(),
-          proficiency: new FormControl()
-        }
-      )
+  fullNameLength=0;
+  constructor(private fb:FormBuilder) { 
+    this.employeeForm=this.fb.group({
+      fullName: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['']
+      })
     })
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.employeeForm.controls['fullName'].valueChanges.subscribe(
+      (value:string)=>this.fullNameLength=value.length
+    )
+  }   
+    // this.employeeForm.valueChanges.subscribe((value:any)=>{
+    //   console.log(JSON.stringify(value));
+    // })
+    // this.employeeForm.get('skills')?.valueChanges.subscribe((value:any)=>{
+    //   console.log(JSON.stringify(value));
+    // })
+  
 
   onLoadDataClick() : void
   {
